dbfbbc3bfae6ce278ec567510727c39f5697a294
mohamedelbarrak <mohamedelbarrak@outlook.fr>
2022-03-22 11:20:33 +0100

    Initial Commit

---
+++ app6_getURL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app6_getURL
+//
+//  Created by E-challenge 2022 on 22/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app6_getURLUITests/app6_getURLUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  app6_getURLUITestsLaunchTests.swift
+//  app6_getURLUITests
+//
+//  Created by E-challenge 2022 on 22/3/2022.
+//
+
+import XCTest
+
+class app6_getURLUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
